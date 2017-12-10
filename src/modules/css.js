b20c1e976b0f1a43ca21b609af3f6619a7106071
zhuweiyou <i@zhuweiyou.com>
2017-12-10 15:45:13 +0800

    调整细节

--- src/modules/css.js
+++ src/modules/css.js
@@ -1,6 +1,6 @@
 module.exports = function (moduleOptions) {
   this.extendBuild((config, {dev}) => {
-    const vueLoader = config.module.rules.find(rule => rule.loader === 'vue-loader')
+    const vueLoader = config.module.rules.find(({loader}) => loader === 'vue-loader')
     vueLoader.options.cssModules = {
       localIdentName: dev ? '[path][name]---[local]---[hash:base64:5]' : '[hash:base64]',
       camelCase: true
