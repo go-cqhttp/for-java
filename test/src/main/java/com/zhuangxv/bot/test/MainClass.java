17bba4ae54114d90d2f5423e7907fd806a963dde
xiaoxu <i@zhuangxv.com>
2024-08-19 15:06:22 +0800

    1、支持官方QQ机器人api

---
+++ test/src/main/java/com/zhuangxv/bot/test/MainClass.java
@@ -0,0 +1,17 @@
+package com.zhuangxv.bot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaoxu
+ * @since 2024/8/19 15:02
+ */
+@SpringBootApplication
+public class MainClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+
+}
